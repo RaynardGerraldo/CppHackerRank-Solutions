7de297a72daba5c52e947c0a6881f662112cd226
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2021-08-17 00:13:14 +0700

    My solution for https://www.hackerrank.com/challenges/cpp-input-and-output/problem

---
+++ inputoutput.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int firstnum;
+    int secnum;
+    int thirdnum;
+    
+    std::cin >> firstnum >> secnum >> thirdnum;
+    
+    std::cout << firstnum + secnum + thirdnum;
+    
+    return 0;
+}
+
