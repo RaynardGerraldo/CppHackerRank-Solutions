2b2661c8530fe43776d0ea327ff263a5bb69ac8a
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2021-08-29 00:57:07 +0700

    My solution for problem https://www.hackerrank.com/challenges/c-tutorial-for-loop/problem

--- Introduction/forloop.cpp
+++ Introduction/forloop.cpp
@@ -4,15 +4,28 @@ void convert(int inp,int inp2){
     
     std::string result;
     
-    std::string converter[] = {"one","two","three","four","five","six","seven","eight","nine"};
+    std::string converter[] = {"one","two","three","four",
+    "five","six","seven","eight","nine"};
     
+    int countup = inp;
     
     for(int i = inp - 1; i <= inp2 - 1 && i < 9; i++){
         result = converter[i];
-        std::cout << result << std::endl;            
+        std::cout << result << "\n";
     }
+    
+    do{
+        countup++;
+        if(countup > 9 && countup % 2 == 0){
+            std::cout << "even" << "\n";
+        }
+    
+        else if(countup > 9 && countup % 2 == 1){
+            std::cout << "odd" << "\n";
+        }
+        
+    }while(countup < inp2);
 }
-
 
 int main() {
     // Complete the code.
@@ -21,22 +34,6 @@ int main() {
     std::cin >> inp, std::cin >> inp2;
     
     convert(inp,inp2);
-    
-    int i = inp;
-    
-    do{
-        i++;
-        
-        if(i > 9 && i % 2 == 0){
-            std::cout << "even" << std::endl;
-        }
-    
-        else if(i > 9 && i % 2 == 1){
-            std::cout << "odd" << std::endl;
-            
-        }
-        
-    }while(i < inp2);
 
     return 0;
 }
