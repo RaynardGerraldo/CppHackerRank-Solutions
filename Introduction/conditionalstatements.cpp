86500e6d5c1fd50edd267e68a8f9711587325b37
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2021-08-29 01:11:45 +0700

    My solution for https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem

--- Introduction/conditionalstatements.cpp
+++ Introduction/conditionalstatements.cpp
@@ -5,45 +5,21 @@ std::string convert(){
     std::cin >> num;
     
     std::string result;
-    if(num <= 9){
-       switch(num) {
-        case 1:
-            result = "one";
-            break;
-        case 2:
-            result = "two";
-            break;
-        case 3:
-            result = "three";
-            break;
-        case 4:
-            result = "four";
-            break;
-        case 5:
-            result = "five";
-            break;
-        case 6:
-            result = "six";
-            break;
-        case 7:
-            result = "seven";
-            break;
-        case 8:
-            result = "eight";
-            break;
-        case 9:
-            result = "nine";
-            break;
+    
+    std::string convert[] = {"one","two","three","four",
+    "five","six","seven","eight","nine"};
 
-       } 
-        
-    }
+    if(num <= 9){
+        result = convert[num - 1];
+    } 
     
     else{
-        std::cout << "Greater than 9" << std::endl;
+        std::cout << "Greater than 9";
     }
+    
     return result;
 }
+
 
 int main(){
     std::cout << convert();
