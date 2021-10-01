b3db02054774798b9f7b29db379cfb1facba04ff
RaynardGerraldo <RaynardGerraldo@users.noreply.github.com>
2021-10-01 18:26:54 +0700

    My solution for https://www.hackerrank.com/challenges/c-tutorial-basic-data-types/problem

---
+++ Introduction/basicdatatypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <cstdio>
+
+int main() {
+    // Complete the code.
+    std::string var;
+    for(int i = 0; i < 5; i++){
+        std::cin >> var;
+        std::cout << var << "\n";
+    }
+    return 0;
+}
+
